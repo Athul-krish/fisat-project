53bd9809e89b62d7cf8f2324d509efd146c6fa65
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-11 10:57:42 +0530

    Delete Student Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import AddStudent from './components/AddStudent'
+import DeleteStudent from './components/DeleteStudent'
 import SearchStudent from './components/SearchStudent'
+import ViewAllStudent from './components/ViewAllStudent'
 
 function App() {
   return (
     <>
       <AddStudent/>
       <SearchStudent/>
+      <ViewAllStudent/>
+      <DeleteStudent/>
     </>
   )
 }
